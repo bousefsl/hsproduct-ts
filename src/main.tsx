1db5e9ba02385dd5cbbaca2439e37deb1001832e
Scott Bousefield <scott.bousefield.z99@hsgb.co.uk>
2025-05-24 19:33:43 +0100

    rootlayout added, routes updated and files added

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { router } from "./router"
 //Style
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.js"
-import "./App.scss"
+import "../src/assets/scss/bootstrap-theme.scss"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,20 @@
-import Home from "./pages/Home"
+//React Router
 import { createBrowserRouter } from "react-router"
+//Layouts
+import RootLayout from "./layouts/RootLayout"
+//Pages
+import Home from "./pages/Home"
+import Comparison from "./pages/Comparison"
+import Product from "./pages/Product"
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "comparison", element: <Comparison /> },
+      { path: "product", element: <Product /> },
+    ],
   },
 ])

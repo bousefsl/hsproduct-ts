f375e2914e0291eb4fb054d7c6e41663bbb153f9
Scott Bousefield <scott.bousefield.z99@hsgb.co.uk>
2025-05-29 17:11:13 +0100

    product filtering added

--- src/pages/Comparison.tsx
+++ src/pages/Comparison.tsx
@@ -1,5 +1,6 @@
+import { useEffect, useRef } from "react"
 //React Router
-import { useLoaderData } from "react-router"
+import { Form, useLoaderData } from "react-router"
 //Components
 import ComparisonCard from "../components/ComparisonCard"
 //Loaders
@@ -8,12 +9,37 @@ import { comparisonProductsLoader } from "../loaders/comparison-products"
 import type { CompProductProps } from "../types"
 
 export default function Comparison() {
-  const { products } = useLoaderData()
+  const {
+    products,
+    searchParams: { productCategory },
+  } = useLoaderData()
   //console.log(products)
+
+  const productRef = useRef<HTMLSelectElement | null>(null)
+
+  useEffect(() => {
+    productRef.current = productCategory || ""
+  }, [productCategory])
 
   return (
     <>
       <h1>Comparison Page</h1>
+
+      <Form>
+        <div className="row g-4">
+          <div className="form-group">
+            <label className="form-label" htmlFor="productCategory">
+              Filter
+            </label>
+            <select className="form-select" id="productCategory" name="productCategory" ref={productRef}>
+              <option>Plumbing</option>
+              <option>Heating</option>
+              <option>Electrics</option>
+            </select>
+            <button className="btn btn-primary">Filter</button>
+          </div>
+        </div>
+      </Form>
 
       <div className="row row-cols-1 row-cols-md-4 g-4">
         {products.length > 0 ? (
@@ -24,16 +50,18 @@ export default function Comparison() {
           <div>Sorry, but there isn't anything matching. Please try again.</div>
         )}
       </div>
-
-      {/* <p>{JSON.stringify(products)}</p> */}
     </>
   )
 }
 
-async function loader() {
-  const products = comparisonProductsLoader()
+async function loader({ request: { signal, url } }: any) {
+  const searchParams = new URL(url).searchParams
+  const productCategory = searchParams.get("productCategory")
+  const filterParams = { productCategory: productCategory }
 
-  return { products: await products }
+  const products = comparisonProductsLoader({ signal, params: filterParams })
+
+  return { products: await products, searchParams: { productCategory } }
 }
 
 export const comparisonListRoute = {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,12 @@ export default function Home() {
   return (
     <>
       <h1 className="text-secondary bg-primary">Homepage</h1>
-      <Link to={`comparison?query=&productCategory=plumbing`}>Comparison</Link>
+      <p>
+        <Link to={`comparison?productCategory=Plumbing`}>Comparison</Link>
+      </p>
+      <p>
+        <Link to={`landlord-comparison?productCategory=Plumbing`}>Landlord Comparison</Link>
+      </p>
     </>
   )
 }

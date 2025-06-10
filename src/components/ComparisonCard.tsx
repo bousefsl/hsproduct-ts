f230cd8a734232f07e3ad48f1d68c41bcf7e9392
Scott Bousefield <scott.bousefield.z99@hsgb.co.uk>
2025-06-10 17:09:58 +0100

    comparison cards style update

--- src/components/ComparisonCard.tsx
+++ src/components/ComparisonCard.tsx
@@ -9,35 +9,39 @@ import formatCurrency from "../utilities/formatCurrency"
 export default function ComparisonCard({ title, monthlyCost, excessCost, inclusions, exclusions, policyLink }: CompProductProps) {
   return (
     <div className="col">
-      <div className="card h-100">
+      <div className="card comparison-card h-100">
         <div className="card-header">{title}</div>
-        <div className="card-body">
-          <div>{formatCurrency(monthlyCost)} a month</div>
-          <div>Excess: £{excessCost}</div>
-          <div>
-            <p>This policy covers:</p>
-            <ul>
-              {inclusions.map((inclusion, index) => {
-                return <li key={index}>{inclusion}</li>
-              })}
-            </ul>
+        <div className="card-body homeserve-medium">
+          <div className="text-center mb-4">
+            <div className="h5 fw-bold">{formatCurrency(monthlyCost)} a month</div>
+            <div>Excess: £{excessCost}</div>
           </div>
-          <div>
-            {exclusions.length > 0 ? (
-              <Fragment>
-                <hr></hr>
-                <div>
-                  <p>Other policies are available which also cover:</p>
-                  <ul>
-                    {exclusions.map((exclusion, index) => {
-                      return <li key={index}>{exclusion}</li>
-                    })}
-                  </ul>
-                </div>
-              </Fragment>
-            ) : (
-              <Fragment></Fragment>
-            )}
+          <div className="policy-attribute-list-wrapper">
+            <div>
+              <p>This policy covers:</p>
+              <ul className="included">
+                {inclusions.map((inclusion, index) => {
+                  return <li key={index}>{inclusion}</li>
+                })}
+              </ul>
+            </div>
+            <div>
+              {exclusions.length > 0 ? (
+                <Fragment>
+                  <hr></hr>
+                  <div>
+                    <p className="text-gray">Other policies are available which also cover:</p>
+                    <ul className="excluded">
+                      {exclusions.map((exclusion, index) => {
+                        return <li key={index}>{exclusion}</li>
+                      })}
+                    </ul>
+                  </div>
+                </Fragment>
+              ) : (
+                <Fragment></Fragment>
+              )}
+            </div>
           </div>
           <div className="text-center">
             <Link to={`../products/${policyLink}`} className="btn btn-primary">

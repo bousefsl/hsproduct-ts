15b5f69cba8f772cae95b1e3d136f72d9ac19e73
Scott Bousefield <scott.bousefield.z99@hsgb.co.uk>
2025-06-19 14:22:04 +0100

    Product carousel added to homepage

---
+++ src/components/ProductsServices.tsx
@@ -0,0 +1,35 @@
+//Embla Carousel
+import EmblaCarousel from "../components/carousel/EmblaCarousel"
+import { EmblaOptionsType } from "embla-carousel"
+//Styles
+import "../assets/css/embla.css"
+
+const OPTIONS: EmblaOptionsType = { align: "center", dragFree: true, loop: true }
+const SLIDE_COUNT = 7
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
+export default function ProductsServices() {
+  return (
+    <div className="products-services">
+      <section className="section-spacer">
+        <div className="container-xl">
+          <div className="row">
+            <div className="col">
+              <h2 className="text-center">Our products and services</h2>
+              <div className="row justify-content-center">
+                <div className="col-md-10 col-lg-8">
+                  <p className="homeserve-medium text-start text-md-center mb-5">Vim te porro labores maluisset, id possim equidem detracto has. Per ne nisl exerci ornatus, eu semper sanctus duo. Eos inani lucilius partiendo ut, ea ius error minimum fierent.</p>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col">
+                  <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@
 import MetaTags from "../components/global/MetaTags"
 import WhyChooseUs from "../components/global/WhyChooseUs"
 import HomeHeroBanner from "../components/HomeHeroBanner"
+import ProductsServices from "../components/ProductsServices"
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <MetaTags title="Demo project with React & Typescript" keywords="lorem, ipsum, sit, dolor, amet" description="Lorem ipsum sit dolor amet" name="Scott" />
       {/* SEO -- End */}
       <HomeHeroBanner />
+      <ProductsServices />
       <WhyChooseUs />
     </>
   )
